b8401ebae2f4d0a78064e984229960a031a3da09
Wesley-waka <wesley.waka@student.moringaschool.com>
2022-11-28 14:48:38 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+// document.body.remove("#main");
+// document.body.createElement('h1#victory');
+// document.body.createElement('h1#victory');
+
+function removeMain() {
+    const main = document.getElementById("main");
+    main.remove();
+}
+
+removeMain();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+newHeader.innerHTML = "Wesley is the champion";
